bcef8d47c9da4471f792051f1e1788c42ae32d33
Fernando-Scroppo <fernandoscroppo@yahoo.com.ar>
2019-12-07 14:42:07 -0300

    Update player.py

--- Juego/player.py
+++ Juego/player.py
@@ -9,18 +9,19 @@ class Goku(pygame.sprite.Sprite):
         self.sheet = pygame.image.load("Imagenes/gokus.png").convert_alpha()         # Cargamos la imagen completa de todos los movimientos
         self.sheet.set_clip(pygame.Rect(351,0,55,94))   #Colocamos las coordenadas para que tome la imagen que va tomar al inicio (posicion x, posicion y, ancho , alto) 
         self.image = self.sheet.subsurface (self.sheet.get_clip()) # Le decimos que tome el rectangulo de las coordenadas que le pasamos                                               
-        # self.image=pygame.transform.scale(image,) tendriamos que tener una variable de ancho y largo de la imagen para poder rescalarla.
+        #self.image=pygame.transform.scale(image) tendriamos que tener una variable de ancho y largo de la imagen para poder rescalarla.
         self.rect = self.image.get_rect()    #Genera un rectangulo alrededor de la imagen que nos va a permitir hacer las colisiones y moverlo.
         self.rect.topleft = position   #Guardamos en la variable position la posicion de nuestro rectangulo.    
         self.frame = 0  #Definimos este primer rectangulo como el frame 0.
         self.left_states = {0:(506,98,70,90)}   #Estado para ir a la izquierda
-        self.right_states = {0: (374,105,73,80)}
         
+        self.right_states = {0: (374,105,73,80)}
         self.up_states = {0:(691,452,55,99)}
         self.down_states = {0:(175,77,46,69)}
         self.stan_parado = {0: (351,0,55,94)}
         self.stan_volar = {0: (288,0,65,101)}
         self.stan_frenar = {0: (330,106,41,72)}
+
     def get_frame(self, frame_set):          #Obtenemos los frame y los va recorriendo segun el estado.
         self.frame += 1
         if self.frame > (len(frame_set) - 1):
